472f149d8e69949f9d212e78dd5833b7cceee836
Michael Pennington <mwpennington@live.com>
2022-07-11 21:34:01 -0500

    got a button and console warn

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
+// import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  // const [joke, setJoke] = useState();
+
+  const getTheJoke = () => {
+    getJoke().then((obj) => {
+      console.warn(obj.setup);
+      console.warn(obj.delivery);
+    });
+  };
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Welcome Home!</h1>
-    </div>
+    <>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          maxWidth: '400px',
+          margin: '0 auto',
+        }}
+      >
+        <h1>Get Joke</h1>
+        <Button type="button" onClick={getTheJoke}>Get Joke</Button>
+      </div>
+    </>
   );
 }
 
